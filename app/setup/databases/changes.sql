e18d787ed1faf47024e554b7a052e2079bf23072
David Weston <westie@typefish.co.uk>
2014-08-12 12:55:36 +0100

    db

--- app/setup/databases/changes.sql
+++ app/setup/databases/changes.sql
@@ -3,47 +3,53 @@
 -- about permissions... thankfully.
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `perm_templ` tinyint(4) NOT NULL,
-  `active` tinyint(4) NOT NULL,
-  `use_ldap` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`)
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`username` varchar(64) NOT NULL,
+	`password` varchar(128) NOT NULL,
+	`fullname` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`description` text NOT NULL,
+	`perm_templ` tinyint(4) NOT NULL,
+	`active` tinyint(4) NOT NULL DEFAULT '1',
+	`use_ldap` tinyint(4) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `zones` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `domain_id` int(11) NOT NULL,
-  `owner` int(11) NOT NULL,
-  `comment` text,
-  `zone_templ_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `domain_id` (`domain_id`),
-  KEY `owner` (`owner`),
-  CONSTRAINT `domain` FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`) ON DELETE CASCADE
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`domain_id` int(11) NOT NULL,
+	`owner` int(11) NOT NULL,
+	`comment` text,
+	`zone_templ_id` int(11) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `domain_id` (`domain_id`),
+	KEY `owner` (`owner`),
+	CONSTRAINT `domain` FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `zone_templ` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
-  `descr` text NOT NULL,
-  `owner` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(128) NOT NULL,
+	`descr` text NOT NULL,
+	`owner` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `zone_templ_records` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `zone_templ_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `type` varchar(6) NOT NULL,
-  `content` varchar(255) NOT NULL,
-  `ttl` int(11) NOT NULL,
-  `prio` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `template` (`zone_templ_id`),
-  CONSTRAINT `template` FOREIGN KEY (`zone_templ_id`) REFERENCES `zone_templ` (`id`) ON DELETE CASCADE
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`zone_templ_id` int(11) NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`type` varchar(6) NOT NULL,
+	`content` varchar(255) NOT NULL,
+	`ttl` int(11) NOT NULL,
+	`prio` int(11) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `template` (`zone_templ_id`),
+	CONSTRAINT `template` FOREIGN KEY (`zone_templ_id`) REFERENCES `zone_templ` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+START TRANSACTION;
+	INSERT INTO `users` (`id`, `username`, `password`, `fullname`, `email`, `active`)
+	VALUES
+		(null, "admin", SHA1("ifacetherisk"), "David Weston", "outragedns@typefish.co.uk", 1);
+COMMIT;
